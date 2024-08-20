73e16d977928b6277ce6d6ec11a9e4acb755f6ad
Michael Roth <mroth@morselife.org>
2024-08-20 09:11:32 -0400

    added Header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Header from "./components/Header";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <Container className="m-3">
+      <Row>
+        <Col>
+          <Header />
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+
+function Header() {
+  return <Button>Create Component</Button>;
+}
+
+export default Header;
